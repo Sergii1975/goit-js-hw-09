cd6a5772cb9c8067de58faf8a18ae5e702bcd4a8
Sergii <svbulgakov75@gmail.com>
2023-05-17 21:14:31 +0300

    allfirst

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const dataStartEl = document.querySelector('[data-start]');
+const dataStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let timerId = null;
+
+dataStartEl.addEventListener('click', onStart);
+dataStopEl.addEventListener('click', onStop);
+
+
+function onStart() {
+  timerId = setInterval(randomColor, 1000);
+  dataStartEl.toggleAttribute('disabled');
+  dataStopEl.removeAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(timerId);
+  dataStartEl.removeAttribute('disabled');
+  dataStopEl.toggleAttribute('disabled');
+}
+
+function randomColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
